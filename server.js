00edbd3cac855330e9a4c846530a249f919f4692
Leo Hu <leo.hu@elotouch.com>
2018-03-04 08:20:56 -0800

    Add projects page

--- server.js
+++ server.js
@@ -51,6 +51,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs',  {
+        pageTitle: 'Project Portfolia'
+    });
+});
+
 app.get('/help', (req, res) => {
     res.send('About Page');
 });
